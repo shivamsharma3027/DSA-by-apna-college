c17a42d606faef062cfad724f7f92a55b285f969
Java DSA by apna college <sharmashivam6585@gmail.com>
2023-12-03 11:59:42 +0530

    change

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,10 +1,1 @@
- n = 334534534;
-        // int rev = 0;
-        // while (n > 0) {
-        //     int rem = n % 10;
-        //     rev = rev * 10 + rem;
-        //     n = n / 10;
-        //     // System.out.print(rem);
-
-        // }
-        // System.out.print(rev);
+// heloo i shivam
